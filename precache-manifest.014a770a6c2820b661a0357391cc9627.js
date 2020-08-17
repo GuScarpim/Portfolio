376904f663fbf2d59d21cc893a2d21c82a273071
Gustavo Scarpim <gustavoscarpim@gmail.com>
2020-08-17 01:05:33 -0300

    Updates

---
+++ precache-manifest.014a770a6c2820b661a0357391cc9627.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b51d5a27bd8e21b1feb8aec3895b6c3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bdbf9eb2bdfc4a7aaa7b",
+    "url": "/Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "34cdeebb94ed446eed9a",
+    "url": "/Portfolio/static/css/main.f2783cf4.chunk.css"
+  },
+  {
+    "revision": "bdbf9eb2bdfc4a7aaa7b",
+    "url": "/Portfolio/static/js/2.0034cfcd.chunk.js"
+  },
+  {
+    "revision": "590b01c142e36ee255242fb9dcd9fd8e",
+    "url": "/Portfolio/static/js/2.0034cfcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34cdeebb94ed446eed9a",
+    "url": "/Portfolio/static/js/main.a3014a71.chunk.js"
+  },
+  {
+    "revision": "16db5c5867658fa7acd2",
+    "url": "/Portfolio/static/js/runtime-main.abe3c866.js"
+  },
+  {
+    "revision": "f57519e0b8d341005f722d6b7f26a12b",
+    "url": "/Portfolio/static/media/ReactNativeCep-cropet.f57519e0.gif"
+  },
+  {
+    "revision": "b92871a7a90245cd9ef8c2cd1eee14aa",
+    "url": "/Portfolio/static/media/api.b92871a7.gif"
+  },
+  {
+    "revision": "6610233836d5ebc867c4d1e43a5c5537",
+    "url": "/Portfolio/static/media/locadora.66102338.gif"
+  },
+  {
+    "revision": "0d1157c23b949b594182cfca465a95d0",
+    "url": "/Portfolio/static/media/next.0d1157c2.svg"
+  },
+  {
+    "revision": "234a5b15adf084bcaab0a82bf7fd8d75",
+    "url": "/Portfolio/static/media/next_google.234a5b15.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.523fa6a09180701d1461d858a0513eb3.js"
+  "/Portfolio/precache-manifest.014a770a6c2820b661a0357391cc9627.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
